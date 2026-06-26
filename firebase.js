c5aebebfec02b6ebe4186167bcf5f721f0c5b072
aziz00455 <heeseok.jung@gmail.com>
2026-06-25 21:47:47 -0400

    R32

--- firebase.js
+++ firebase.js
@@ -15,5 +15,5 @@ const db = firebase.firestore();
 // **** Instance Identifyer ****
 const GAME_ID = "WC2026_DEV";
 
-const R32_SELECTION = "User"; // allowed: "Auto" or "User"
+const R32_SELECTION = "Auto"; // allowed: "Auto" or "User"
 
